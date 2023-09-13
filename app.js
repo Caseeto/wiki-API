a5103c11513e5f7289ac057f7573b9480741174c
TrexASHUoP <92679039+Caseeto@users.noreply.github.com>
2023-09-13 10:32:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ashishsaranshi:ashuas@cluster0.xriv1c7.mongodb.net/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
   title: String,
